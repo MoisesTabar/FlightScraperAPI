dcb298ef690999eea0dd3721dc6f53bd45cd70b0
Moises Tabar <moisestabar@192.168.1.20>
2026-01-05 20:26:19 -0400

    chore: Added CORS policy for local development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scraper.router import router as scraper_router
 from voice.router import router as voice_router
 from text.router import router as text_router
@@ -11,6 +12,13 @@ app = FastAPI(
 )
 
 register_exception_handlers(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(scraper_router, prefix='/flights')
 app.include_router(voice_router, prefix='/flights')
